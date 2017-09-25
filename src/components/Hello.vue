798370970a07e7d6ccbca82c26743dffa01ea62c
savokiss <jaynaruto@qq.com>
2017-09-25 14:15:27 +0800

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <video-player class="vjs-custom-skin" ref="videoPlayer" :options="playerOptions">
+    </video-player>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      playerOptions: {
+        autoplay: false,
+        controls: true,
+        techOrder: ['flash', 'html5'],
+        sourceOrder: true,
+        flash: { hls: { withCredentials: false } },
+        html5: { hls: { withCredentials: false } },
+        sources: [{
+          type: 'rtmp/mp4',
+          src: 'rtmp://184.72.239.149/vod/&mp4:BigBuckBunny_115k.mov'
+        }, {
+          withCredentials: false,
+          type: 'application/x-mpegURL',
+          src: 'http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8'
+        }],
+        poster: 'assets/logo.png',
+        // controlBar: {
+        //   timeDivider: false, // 时间分割线
+        //   durationDisplay: false, // 总时间
+        //   progressControl: true, // 进度条
+        //   customControlSpacer: true, // 未知
+        //   fullscreenToggle: true // 全屏
+        // },
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueVideoPlayer from 'vue-video-player'
+import 'video.js/dist/video-js.css'
+import 'vue-video-player/src/custom-theme.css'
+import 'videojs-flash'
+import 'videojs-contrib-hls'
+
+Vue.config.productionTip = false
+Vue.use(VueVideoPlayer)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
