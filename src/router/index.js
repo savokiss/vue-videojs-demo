5edd36c4e6560f6a0cbd035bb05248f50bf0423e
savokiss <jaynaruto@qq.com>
2017-11-13 16:59:09 +0800

    feat: normal video

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-
+import home from '@/views/home'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    }, {
+      path: '/home',
+      name: 'home',
+      component: home
     }
   ]
 })
---
+++ src/views/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="homeView">
+    <video-player class="vjs-custom-skin" ref="videoPlayer" :options="playerOptions">
+    </video-player>
+
+    <div class="optionsWrapper">
+      <input type="text" v-model="url">
+      <button @click="handleApply">应用</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      url: '',
+      playerOptions: {
+        autoplay: false,
+        controls: true,
+        sources: [{
+          type: 'video/mp4',
+          src: 'http://vjs.zencdn.net/v/oceans.mp4'
+        }]
+      }
+    }
+  },
+  methods: {
+    handleApply () {
+      this.playerOptions.sources[0].src = this.url
+    }
+  }
+}
+</script>
+
+<style scoped>
+.optionsWrapper {
+  margin-top: 20px;
+}
+</style>
